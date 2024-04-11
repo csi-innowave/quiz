479d984baed39e53a524e9627f42e1189918fdec
Yash <yg232004@gmail.com>
2024-04-11 18:25:54 +0530

    added next prev

---
+++ src/components/Questions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Questions = () => {
+  return <div className="text-white">Questions</div>;
+};
+
+export default Questions;
--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,7 +1,41 @@
-import { BackgroundBeams } from "./ui/background-beams";
+import Questions from "./Questions";
+import { WavyBackground } from "./ui/wavy-background";
 
 const Quiz = () => {
-  return <>Hi</>;
+  function handlePrev() {
+    console.log("Previous clicked");
+  }
+  function handleNext() {
+    console.log("Next Clicked");
+  }
+  return (
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        MindForge Round I
+      </p>
+      <Questions />
+      <div className="grid grid-cols-2 mt-[70%] gap-20">
+        <button
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          onClick={handlePrev}
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Previous
+          </span>
+        </button>
+        <button
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          onClick={handleNext}
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Next
+          </span>
+        </button>
+      </div>
+    </WavyBackground>
+  );
 };
 
 export default Quiz;
