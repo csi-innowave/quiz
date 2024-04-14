87c20bd666d08d301657dac9eaf89dedba52a8bb
Yash <yg232004@gmail.com>
2024-04-15 01:56:06 +0530

    fixed home ui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,40 +25,39 @@ const Home = () => {
         <div className="relative md:gap-10 gap-5 z-20 pt-5 flex justify-center items-center mx-auto">
           <img
             src="https://res.cloudinary.com/dpp2rltxx/image/upload/v1713109149/csi/qyuz/hfumyw1sacge8ooeqaro.png"
-            className="md:h-32 h-24"
+            className="md:h-40 h-24"
           />
           <img
             src="https://res.cloudinary.com/dpp2rltxx/image/upload/v1709819749/csi/uwmdhwkpunbabkatohsf.png"
-            className="md:h-24 h-20"
+            className="md:h-36 h-20"
           />
           <img
             src="https://res.cloudinary.com/dpp2rltxx/image/upload/v1713109150/csi/qyuz/x4kj6d42uqlrcdzg2mvb.png"
-            className="md:h-24 h-20"
+            className="md:h-36 h-20"
           />
         </div>
 
-        <div className="flex justify-center items-center flex-col pt-[5%]">
+        <div className="flex justify-center items-center flex-col pt-[2%] md:pt-0 ">
           <div>
-            <h1 className="text-white font-fjalla">Welcome To The Quiz</h1>
-
-            <div className="text-white text-xl font-jersey">
-              Text with Jersey-15 font
-            </div>
-            <div className="text-white text-xl font-spartan">
-              Text with Jersey-15 font
-            </div>
+            <h1 className="md:text-[9rem] text-8xl text-center font-jersey text-white text-glow">
+              WELCOME
+            </h1>
+            <h1 className="md:text-8xl text-5xl text-center font-extrabold font-spartan text-gray-300 ">
+              TEST YOUR KNOWLEDGE
+            </h1>
           </div>
           <input
             ref={inputRef}
-            placeholder="Enter Username"
-            className="rounded-lg px-2 bg-slate-800 mt-5 z-10  mb-2 py-2 shadow-2xl shadow-zinc-900 text-lg font-sans font-semibold text-white"
+            placeholder="ENTER USERNAME"
+            className="rounded-xl text-white placeholder-white text-center px-2 bg-black bg-opacity-70 md:bg-transparent mt-5 mb-2 py-2 text-3xl font-semibold font-fjalla 
+    shadow-white"
           />
         </div>
-        <Link to={"/quiz"} className="relative flex justify-center top-40">
+        <Link to={"/quiz"} className="relative flex justify-center pt-[2%]">
           <button onClick={startQuiz} className="p-[3px] relative">
-            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-            <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-              Get Started
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-xl" />
+            <div className="px-8 py-2 text-2xl font-fjalla bg-black rounded-xl  relative group transition duration-200 text-white hover:bg-transparent">
+              G E T <br /> S T A R T E D
             </div>
           </button>
         </Link>
