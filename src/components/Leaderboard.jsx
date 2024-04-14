29c0333f7c73867ee158238810413953f28b3194
joyalljijo <joyal2405@gmail.com>
2024-04-15 00:53:07 +0530

    revamped leaderboard ui

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,18 +1,17 @@
-import { BackgroundBeams } from "./ui/background-beams";
+
 import ResultTable from "./resultTable";
-import "./ui/leaderboard.css";
 const Leaderboard = () => {
   return (
-    <div>
-      <div className="h-screen w-full relative pt-5 antialiased ">
-        <div className="max-w-2xl mx-auto p-4">
-          <h1 className="relative z-10 text-3xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-blue-200 to-neutral-600  text-center font-sans font-bold">
-            LeaderBoard
-          </h1>
-          <ResultTable />
+      <>
+        <div className=" bg-contain bg-repeat" style={{backgroundImage: `url('https://res.cloudinary.com/dfhj4i9hd/image/upload/v1713119651/CSI%20Website/Quiz/background_pdy1uy.png')`}}>
+          <div className="mx-44">
+            <h1 className="text-3xl md:text-7xl  text-white text-center font-bold mb-7 pt-7">
+              LeaderBoard
+            </h1>
+            <ResultTable/>
+          </div>
         </div>
-      </div>
-    </div>
+      </>
   );
 };
 
